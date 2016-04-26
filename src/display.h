796549adfc7fc22975ee939b4cd50b0a0cdbb417
Loris Lucido <loris.lucido@gmail.com>
2016-04-26 22:00:29 +0200

    byebye warnings

--- src/display.h
+++ src/display.h
@@ -8,7 +8,7 @@ typedef unsigned **sand_t;
 typedef unsigned (*get_func_t) (unsigned x, unsigned y,
 				sand_t sand);
 
-typedef bool (*compute_func_t) (sand_t sand);
+typedef int (*compute_func_t) (sand_t sand);
 
 typedef float * (*iterate_func_t) (compute_func_t compute_func,
 				   unsigned iterations,
